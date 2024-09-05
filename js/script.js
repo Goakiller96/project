2e17a31232b0a15a085a099872bcab335c960942
andrey <kiseleffav@gmail.com>
2024-09-05 16:25:38 +0500

    05.09.24

---
+++ js/script.js
@@ -0,0 +1,33 @@
+// let result = '';
+// const length = 10;
+
+// for (let index = 1; index < length; index++) {
+//     for (let j = 0; j < index; j++) {
+//         result += '*';
+//     }
+//     result += '\n';
+// }
+
+// console.log(result);
+
+
+
+
+// Средний возраст, Студента с самой высокой оценкой, самой низкой средней оценкой
+
+const students = [
+    { name: 'Алиса', age: 22, grades: [90, 85, 88] },
+    { name: 'Боб', age: 20, grades: [70, 75, 72] },
+    { name: 'Чарли', age: 23, grades: [95, 90, 93] },
+];
+
+
+
+for (let i = 0, l = students.length; i < 3; i++) {
+    console.log(students[i].age);
+};
+
+
+
+
+
